71fd98f9b1a631d0bde5e358d6fbff05939ffbee
Manish Yadav <manish@Manishs-MacBook-Pro.local>
2023-03-12 19:37:17 +0530

    added local files

---
+++ main.tf
@@ -0,0 +1,189 @@
+# Define provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create subnets
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private"
+  }
+}
+
+# Create security groups
+resource "aws_security_group" "public" {
+  name_prefix = "public"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private" {
+  name_prefix = "private"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = [aws_security_group.public.id]
+  }
+}
+
+# Create EC2 instances
+resource "aws_instance" "web" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  security_groups = [aws_security_group.public.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              EOF
+}
+
+resource "aws_instance" "app" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.private.id
+  security_groups = [aws_security_group.private.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y docker.io
+              EOF
+}
+
+# Create RDS instance
+resource "aws_db_instance" "postgres" {
+  allocated_storage = 10
+  engine = "postgres"
+  engine_version = "11.4"
+  instance_class = "db.t2.micro"
+  db_name = "mydb"
+  username = "admin"
+  password = "password"
+  parameter_group_name = "default.postgres11"
+  skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.private.id]
+  db_subnet_group_name = "mydb-subnet-group"
+
+  tags = {
+    Name = "mydb"
+  }
+}
+
+# Create ELB
+resource "aws_elb" "main" {
+  name = "my-elb"
+  subnets = [aws_subnet.public.id]
+  security_groups = [aws_security_group.public.id]
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+    }
+}
+
+# Create S3 bucket for static files
+resource "aws_s3_bucket" "static" {
+  bucket = "manish-static-files"
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+}
+
+# Create CloudFront distribution
+resource "aws_cloudfront_distribution" "main" {
+  origin {
+    domain_name = aws_s3_bucket.static.bucket_regional_domain_name
+    origin_id = "S3-manish-static-files"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "S3-manish-static-files"
+    
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+  price_class = "PriceClass_100"
+  aliases = ["www.manish.com", "manish.com"]
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+#Create Route53 record
+
+resource "aws_route53_zone" "main" {
+name = "manish.com"
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.main.id
+  name = "manish.com"
+  type = "A"
+  alias {
+    name = aws_cloudfront_distribution.main.domain_name
+    zone_id = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
